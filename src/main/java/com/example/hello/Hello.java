511a8e9982e9f07de1aa8b2ab3df585627b5fb42
sabriajis <ajissabri@gmail.com>
2024-05-02 14:55:52 +0700

    git init hello maven

---
+++ src/main/java/com/example/hello/Hello.java
@@ -0,0 +1,17 @@
+
+package com.example.hello;
+
+import org.springframework.web.bind.annotation.RequestMapping;  
+import org.springframework.web.bind.annotation.RestController;  
+   
+
+@RestController  
+    public class Hello {  
+   
+        @RequestMapping("/")  
+    
+        public String hello(){  
+    
+            return "Hello nando";  
+    }  
+    }  
